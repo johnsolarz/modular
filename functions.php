5e8380c4f54f952d744c0f541c53ca3c0ab61cef
John Solarz <johnsolarz@gmail.com>
2013-09-13 13:12:26 -0500

    Move dashboard functions and assets to plugin

--- functions.php
+++ functions.php
@@ -18,4 +18,3 @@ require_once locate_template('/lib/relative-urls.php');   // Root relative URLs
 require_once locate_template('/lib/widgets.php');         // Sidebars and widgets
 require_once locate_template('/lib/scripts.php');         // Scripts and stylesheets
 require_once locate_template('/lib/custom.php');          // Custom functions
-require_once locate_template('/lib/dashboard.php');       // ESC login, dashboard and widgets
