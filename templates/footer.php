54e81ebe7e0217c1499da6156442b533abd86364
John Solarz <johnsolarz@gmail.com>
2014-07-25 09:32:49 -0500

    Add byline

--- templates/footer.php
+++ templates/footer.php
@@ -1,6 +1,7 @@
 <footer class="content-info" role="contentinfo">
   <div class="container">
     <?php dynamic_sidebar('sidebar-footer'); ?>
+    <p>Site by <a href="http://eightsevencentral.com" target="_blank">Eight Seven Central</a></p>
   </div>
 </footer>
 
